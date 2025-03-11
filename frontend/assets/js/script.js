55b14b5787979495688a95acb1a2c096c5cfd1a6
Adrian Khlim <adriankhlimusa@gmail.com>
2025-03-11 04:09:26 -0400

    Updated home page

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,17 +1,15 @@
 document.getElementById("loadDataBtn").addEventListener("click", function() {
-    fetch("http://127.0.0.1:5000/load_data")
+    fetch("http://127.0.0.1:5000/ct")
         .then(response => response.json())
         .then(data => {
-            alert(data.message);
-        });
+            alert(data.message); // Show the response message
+        })
+        .catch(error => console.error("Error:", error));
 });
 
-document.getElementById("computeDoseBtn").addEventListener("click", function() {
-    fetch("http://127.0.0.1:5000/compute_dose")
-        .then(response => response.json())
-        .then(data => {
-            alert(data.message);
-            document.getElementById("doseImage").src = "http://127.0.0.1:5000/static/SimpleDose.png";
-            document.getElementById("doseImage").style.display = "block";
-        });
+
+document.getElementById("viewResultsBtn").addEventListener("click", function() {
+    let imgElement = document.getElementById("doseImage");
+    imgElement.src = "http://127.0.0.1:5000/get_image";
+    imgElement.style.display = "block";
 });
